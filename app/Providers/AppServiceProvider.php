749fee67bb4dddb28431e3c051ca8fc76a74cb07
Mohamed-Elloumi <mohamed.elloumi.med@gmail.com>
2024-10-11 03:11:35 +0200

    Commit new version

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
-/**
-* Bootstrap any application services.
-*
-* @return void
-*/
-public function boot()
+class AppServiceProvider extends ServiceProvider
 {
-Schema::defaultStringLength(191);}
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Set the default string length for database columns
+        Schema::defaultStringLength(191);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
