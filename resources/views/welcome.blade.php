a1fb5ead32746bf5090df367f087dd42d02de8c3
Mohamed-Elloumi <mohamed.elloumi.med@gmail.com>
2024-11-13 11:34:29 +0100

    3emecommit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+       <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
+       <link href="{{ secure_asset('frontend/assets/index-Dv-JLakp.css') }}"rel="stylesheet">
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -16,6 +19,8 @@
         </style>
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <div id="root"></div>
+<script src="{{ secure_asset('frontend/assets/index-BHasKgAD.js')}}"></script>
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
